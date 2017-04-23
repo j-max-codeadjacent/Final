492ec018e4733e2d0fb6b983a74d3888afb5c254
J. Max Barry <jmax.barry@gmail.com>
2017-04-23 16:50:22 -0500

    With large book list, still before dictionary dates

--- iteratebooks.py
+++ iteratebooks.py
@@ -1,7 +1,10 @@
 import fourb_author_objects as fourb
 import nltk
+from openpyxl import Workbook
 
-titles = ["adambede", 'themillonthefloss', 'silasmarner', 'romola', 'felixholt', 'middlemarch', "deronda", "roderickhudson", "thebeastinthejungle", "theinnocentsabroad"]
+titles = ["adambede", 'themillonthefloss', 'silasmarner', 'romola', 'felixholt', 'middlemarch', "deronda", 
+"roderickhudson", "theamericans", 'theeuropeans', 'confidence', 'portraitofaladyvol1', 'portraitofaladyvol2', 'thegoldenbowl', "thebeastinthejungle", 
+"theinnocentsabroad", "roughingit", "theadventuresoftomsawyer", 'lifeonthemississippi', 'aconnecticutyankee', 'thetragedyofpuddnhead', 'joanofarcvol1', 'joanofarcvol2']
 
 texts = {"adambede":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/GeorgeEliot/adambede.txt"),
 "themillonthefloss":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/GeorgeEliot/themillonthefloss.txt"),
@@ -11,9 +14,23 @@ texts = {"adambede":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts
 "middlemarch":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/GeorgeEliot/middlemarch.txt"),
 "deronda": ("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/GeorgeEliot/deronda.txt"),
 "roderickhudson":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/roderickhudson.txt"),
+"theamericans":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/theamericans.txt"),
+"theeuropeans":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/theeuropeans.txt"),
+"confidence":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/confidence.txt"),
+"portraitofaladyvol1":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/portraitofaladyvol1.txt"),
+"portraitofaladyvol2":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/portraitofaladyvol2.txt"),
+"thegoldenbowl":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/thegoldenbowl.txt"),
 "thebeastinthejungle":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/HenryJames/thebeastinthejungle.txt"),
-"theinnocentsabroad":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/theinnocentsabroad.txt")
+"theinnocentsabroad":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/theinnocentsabroad.txt"),
+"roughingit":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/roughingit.txt"),
+"theadventuresoftomsawyer":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/theadventuresoftomsawyer.txt"),
+"lifeonthemississippi":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/lifeonthemississippi.txt"),
+"aconnecticutyankee":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/aconnecticutyankee.txt"),
+"thetragedyofpuddnhead":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/thetragedyofpuddnhead.txt"),
+"joanofarcvol1":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/joanofarcvol1.txt"),
+"joanofarcvol2":("/Users/jmax.barry/Documents/Coding/DataScience/Final/Texts/MarkTwain/joanofarcvol2.txt")
 }
+
 start_end = {'adambede': ('Book One', 'SELECTED BIBLIOGRAPHY'), 
 'themillonthefloss': ('Book I', 'End of the Project Gutenberg EBook'), 
 'silasmarner': ('PART ONE', 'End of the Project Gutenberg EBook'), 
@@ -22,8 +39,21 @@ start_end = {'adambede': ('Book One', 'SELECTED BIBLIOGRAPHY'),
 'middlemarch': ('PRELUDE', 'End of the Project Gutenberg EBook'), 
 'deronda':('BOOK I', 'End of the Project Gutenberg'),
 'roderickhudson':('CHAPTER I', 'End of the Project Gutenberg EBook'),
+'theamericans':('On a brilliant', 'End of the Project Gutenberg EBook'),
+'theeuropeans':('A narrow grave-yard', 'The End'),
+'confidence':('CHAPTER I', 'End of the Project Gutenberg EBook'),
+'portraitofaladyvol1':('Under certain circumstances', 'End of the Project Gutenberg EBook'),
+'portraitofaladyvol2':('CHAPTER XXVIII', 'End of the Project Gutenberg EBook'),
+'thegoldenbowl':('BOOK FIRST: THE PRINCE', 'End of the Project Gutenberg EBook'),
 'thebeastinthejungle':('CHAPTER I', 'End of the Project Gutenberg EBook'),
-'theinnocentsabroad':('CHAPTER I', 'End of the Project Gutenberg EBook')
+'theinnocentsabroad':('CHAPTER I', 'End of the Project Gutenberg EBook'),
+'roughingit':('My brother had just been', 'End of the Project Gutenberg EBook'),
+'theadventuresoftomsawyer':('TOM!', 'End of the Project Gutenberg EBook'),
+'lifeonthemississippi':('The River and Its History', 'End of the Project Gutenberg EBook'),
+'aconnecticutyankee':('The ungentle laws', 'End of the Project Gutenberg EBook'),
+'thetragedyofpuddnhead':('A Whisper', 'Transcriber\'s Notes'),
+'joanofarcvol1':('By The Sieur Louis De Conte', 'End of the Project Gutenberg EBook'),
+'joanofarcvol2':('BOOK II -- IN COURT AND CAMP (Continued)', 'End of the Project Gutenberg EBook')
 }
 
 
@@ -50,6 +80,8 @@ for x in titles:
 	print len(types)
 	t_text = nltk.Text(tokens)
 	type(t_text)"""
+
+
 
 
 
